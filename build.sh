73378552686a5a26c492b8ca1c78b28eea2d5a8f
Antonio Martinez <jose.antonio.martinez.torres@intel.com>
2019-10-23 16:25:04 -0700

    build script checks for docker and Intel rsp controller running

--- build.sh
+++ build.sh
@@ -20,12 +20,33 @@
 echo
 echo "Installing the following dependencies..."
 echo "    1. make"
+echo "    2. curl"
 echo
 apt update
-apt -y install build-essential
+apt -y install build-essential curl
+
+echo
+echo "Checking docker and Intel RSP..."
+echo
+{
+  docker ps -q > /dev/null
+} || {
+  echo "Docker is not running. Please make sure Docker is installed."  
+  exit 1
+}
+echo "Docker...OK"
+echo
+if curl --output /dev/null --silent --head --fail "http://127.0.0.1:8080/web-admin"
+then
+    echo "Intel RSP Controller...OK"
+else
+    echo "Intel RSP is not running. Please follow instructions to install and run RSP controller."
+    exit 1
+fi
 echo
 echo "Building EdgeX and Intel Inventory Suite..."
 echo
+
 if make build 
 then   
   if make deploy 
